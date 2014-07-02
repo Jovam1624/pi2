89faaa5f3f94fbd8b78e56ee4356782980481ecf
linoxydable <abdel.aithadji616@gmail.com>
2014-07-02 17:43:13 -0400

    nouveaux fichiers

---
+++ controleurs/test.php
@@ -0,0 +1,17 @@
+
+<?php
+    if (isset($_GET['cat'])) {
+        $categorie = $_GET['cat'];
+    }
+    else {
+        $categorie = 'Santé';
+    }
+?>
+
+      <article class="enssemble-articles">
+        <?php var_dump('test'); ?>
+  	</article> 
+        
+
+	
+  
